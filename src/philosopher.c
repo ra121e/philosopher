367405f315bbf340843d09f9939bff9fa8c23b2c
root <sushi.honda@gmail.com>
2024-11-26 21:17:31 +0800

    multi philo

--- src/philosopher.c
+++ src/philosopher.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philisopfer.c                                      :+:      :+:    :+:   */
+/*   philosopher.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 18:07:25 by athonda           #+#    #+#             */
-/*   Updated: 2024/11/25 19:44:52 by athonda          ###   ########.fr       */
+/*   Updated: 2024/11/26 21:13:43 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
+#define NUM_PHILO 5
 
 void	*life(void *arg)
 {
@@ -44,16 +45,28 @@ void	*life(void *arg)
 int	main(int ac, char **av)
 {
 	int	i;
-	pthread_t	pt;
-	pthread_t	pt2;
+	pthread_t	pt[NUM_PHILO];
+	int	ret[NUM_PHILO];
+	//pthread_t	pt2;
 
-
-	i = pthread_create(&pt, NULL, &life, (void *)pt);
-	printf("return value: %d\n", i);
-	printf("thread id: %ld\n", pt);
-	sleep(1);
-	i = pthread_create(&pt2, NULL, &life, (void *)pt2);
-	pthread_join(pt, NULL);
-	pthread_join(pt2, NULL);
+	i = 0;
+	while (i < NUM_PHILO)
+	{
+		ret[i] = pthread_create(&pt[i], NULL, &life, (void *)pt[i]);
+		printf("return value: %d\n", ret[i]);
+		printf("thread id: %ld\n", pt[i]);
+		i++;
+	}
+	sleep(10);
+	//i = pthread_create(&pt2, NULL, &life, (void *)pt2);
+	//printf("return value: %d\n", i);
+	//printf("thread id: %ld\n", pt2);
+	i = 0;
+	while (i < NUM_PHILO)
+	{
+		pthread_join(pt[i], NULL);
+		i++;
+	}
+	//pthread_join(pt2, NULL);
 	return (0);
 }
